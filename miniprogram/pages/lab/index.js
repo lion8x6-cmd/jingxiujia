f297b857b2c2921761aee1f00a654dccb7bd2647
234422561 <234422561@qq.com>
2026-08-17 15:53:03 +0800

    fix: 调试Tab选中态索引修复
    - lab/index页selected索引从4改为3（调试tab是第4个，索引3）
    - mine页索引4不变（第5个tab，索引4）

--- miniprogram/pages/lab/index.js
+++ miniprogram/pages/lab/index.js
@@ -19,7 +19,7 @@ Page({
 
   onShow() {
     if (typeof this.getTabBar === 'function' && this.getTabBar()) {
-      this.getTabBar().setData({ selected: 4 });
+      this.getTabBar().setData({ selected: 3 });
     }
   },
 
